28ad4159efb04b891dfc7b610d4b7c5f781e7bc3
devraghavs <dev.raghavsomani@gmail.com>
2021-12-30 12:40:35 +0530

    task1_done

--- robot_order_fixes.go
+++ robot_order_fixes.go
@@ -14,63 +14,38 @@ import (
 
 // StringService provides operations on strings.
 type StringService interface {
-	Order(string) (string, error)
+	
+	Order(string,map[string][]float64) (string, error)
 }
 
 // orderService is a concrete implementation of StringService
 type orderService struct{}
 
-func (orderService) Order(l string) (string, error) {
-	arr:= [10][3]interface{}{
-        {"A",10.28,9},
-        {"B",24.07,7},
-        {"C",13.30,0},
-        {"D",28.94,1},
-        {"E",12.39,3},
-        {"F",30.77,2},
-        {"G",55.13,15},
-        {"H",50.00,7},
-        {"I",90.12,92},
-        {"J",82.31,15},
-    }
-	total := 0.0
-    res1 :=0
-    flag :=0;
-    var flag2 int;
-	if l == "" {
-			return "", ErrEmpty
-		}
-    if len(l)!=4 || !(strings.ContainsAny(l,"a | b | c") && strings.ContainsAny(l,"d | e") && strings.ContainsAny(l,"f | g | h") && strings.ContainsAny(l,"i | j")){
-        return ErrInput,nil;
+func (orderService) Order(l string ,arr map[string][]float64) (string, error) {
+    total := 0.0
+    if len(l)!=4 || !(strings.ContainsAny(l,"A | B | C") && strings.ContainsAny(l,"D | E") && strings.ContainsAny(l,"F | G | H") && strings.ContainsAny(l,"I | J")){
+        return "",ErrEmpty
     }else{
-         for i:=range l{
-             for j:=range arr{
-             res:=strings.Index(arr[j][0].(string),strings.ToUpper(string(l[i])))
-             if res==0{
-                 res1=j
-                 if arr[j][2].(int)>0{
-                    fmt.Println("res1",res1,arr[j][1]) 
-                    total+=arr[res1][1].(float64)
-                    // arr[j][2].(int)=(arr[j][2].(int))-1
-                    break
-                 }else{
-                      flag2=1
-                     break
-                 }}}
-            if flag2<1{
-				continue
+        for i:=range l{
+            key:=string(l[i])
+            if arr[key][1]==0{
+				return "",ErrEmptyPart
+                break
             }else{
-                break}}
-     if flag2<1 && flag!=1{
-		x := fmt.Sprintf("%v", total)
-         return x,nil
-    }else{
-        return ErrEmptyPart,nil}}
+                    total+=arr[key][0]
+                    arr[key][1]=arr[key][1] - 1
+            }
+        }
+        x := fmt.Sprintf("%v", total)
+        return x,nil
+	}
 }
+
+
+
 // ErrEmpty is returned when an input string is empty.
-var ErrEmpty = errors.New("empty string")
-var ErrEmptyPart = "Error : Cannot create robot"
-var ErrInput="Invalid Input:Please enter all types of body part"
+var ErrEmpty = errors.New("Invalid Input")
+var ErrEmptyPart = errors.New("Error : Cannot create robot")
 
 // For each method, we define request and response structs
 type orderRequest struct {
@@ -78,16 +53,28 @@ type orderRequest struct {
 }
 
 type ordercaseResponse struct {
-	V   string `json:"total"`
+	V   string `json:"total,omitempty"`
 	
-	Err string `json:"err,omitempty"` // errors don't define JSON marshaling
+	Err string `json:"Error:,omitempty"` // errors don't define JSON marshaling
 }
 
 // Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
 func makeOrderEndpoint(svc StringService) endpoint.Endpoint {
+	arr:= map[string][]float64{
+        "A":{10.28,9},
+        "B":{24.07,7},
+        "C":{13.30,0},
+        "D":{28.94,1},
+        "E":{12.39,3},
+        "F":{30.77,2},
+        "G":{55.13,15},
+        "H":{50.00,7},
+        "I":{90.12,92},
+        "J":{82.31,15},
+    }
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		req := request.(orderRequest)
-		v, err := svc.Order(req.S)
+		v, err := svc.Order(req.S,arr)
 		if err != nil {
 			return ordercaseResponse{v, err.Error()}, nil
 		}
@@ -97,6 +84,7 @@ func makeOrderEndpoint(svc StringService) endpoint.Endpoint {
 
 // Transports expose the service to the network. In this first example we utilize JSON over HTTP.
 func main() {
+
 	svc := orderService{}
 	orderHandler := httptransport.NewServer(
 		makeOrderEndpoint(svc),
